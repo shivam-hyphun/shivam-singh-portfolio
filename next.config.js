b78396c3bad27866a6c250431432c34dbf98388b
Sagarsingh-lang <ss8839273682sagar@gmail.com>
2024-08-12 22:26:27 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isProd ? '/shivam-singh-portfolio' : '',
-  assetPrefix: isProd ? '/shivam-singh-portfolio/' : '',
+  basePath: isProd ? '/' : '',
+  assetPrefix: isProd ? '/' : '',
   images: {
     unoptimized: true,
   },
