7ad5dd3a4c76997fed973578d4203d1ca3bfa25b
Sagarsingh-lang <ss8839273682sagar@gmail.com>
2024-08-12 21:52:49 +0530

    fix all package json nextjs yml and nextconfig

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-	output: "export",
-	reactStrictMode: true,
-	swcMinify: true,
-	experimental: {
-		images: {
-			unoptimized: true,
-		},
-	},
-	basePath: '',
-	assetPrefix: '',
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: isProd ? '/shivam-singh-portfolio' : '',
+  assetPrefix: isProd ? '/shivam-singh-portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
