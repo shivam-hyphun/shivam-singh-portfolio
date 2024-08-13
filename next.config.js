e6cd434b5dd1cf3406a240a64af00139f0314997
Sagarsingh-lang <ss8839273682sagar@gmail.com>
2024-08-13 08:55:07 +0530

    add config changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isProd ? '/' : '',
-  assetPrefix: isProd ? '/' : '',
+  basePath: isProd ? '/shivam-singh-portfolio' : '',
+  assetPrefix: isProd ? '/shivam-singh-portfolio/' : '',
   images: {
     unoptimized: true,
   },
